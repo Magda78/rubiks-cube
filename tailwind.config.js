481fc0c38e9feb33280d366ff912b6cd2d2ecea8
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2022-06-25 18:34:31 +0200

    Has ts and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'bg-image': "url('/img/')"
-			}
-		}
-	},
-	plugins: []
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
