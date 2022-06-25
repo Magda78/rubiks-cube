9c9b91f82a28a586dbbef27a1e57174e5fed40d7
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2022-06-25 19:21:37 +0200

    Added scramble function

---
+++ utils/scramble.test.ts
@@ -0,0 +1,9 @@
+import { scramble } from './scramble';
+
+describe('scramble()', () => {
+  it('should return a string', () => {
+    const scrambleMoves = scramble();
+
+    expect(scrambleMoves).toBeDefined();
+  });
+});
