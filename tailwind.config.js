bc909fc7f6d69cc374f4615d07043f26dab34a12
Magda <>
2022-06-25 12:12:01 -0400

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'bg-image': "url('/img/')"
+			}
+		}
+	},
+	plugins: []
+};
